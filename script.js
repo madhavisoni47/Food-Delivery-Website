60eced6088b081b2c57178d786c42185f2a0a550
Madhavi soni <smadhvi047@gmail.com>
2026-02-22 04:52:29 +0530

    add slider with using js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 
 var swiper = new Swiper(".mySwiper", {
+      loop:true,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#swiper-button-next",
+        prevEl: "#swiper-button-prev",
       },
     });
+
+
+
+//   let sliderSlide = document.querySelectorAll(".swiper-slide")
+
+//   function swiperRestart (slider){
+//     let i = 0;
+//     setInterval(()=>{git
+
+//       slider.forEach((slide) => {
+//       slide.style.display = "none";
+//     });
+
+//     // Show current slide
+//     slider[i].style.display = "block";
+
+//       i++;
+//       if(i>=slider.length){
+//         i=0;
+//       }
+
+//     }, 3000)
+//   }
+
+// swiperRestart(sliderSlide)
